2cab85cf30ab0a369011da841b79a6a20b23255e
Alex Torres <wildrecon11@gmail.com>
2024-09-23 15:28:39 -0700

    The Base Character Class

---
+++ Aura_UE_Version_5_3/Aura/Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Torres Inc.
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Aura_UE_Version_5_3/Aura/Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Torres Inc.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
